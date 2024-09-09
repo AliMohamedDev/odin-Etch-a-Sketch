910c81dad6003116462c779ef99921e7e3a83ac1
Ali Mohamed <hassanali1910@yahoo.ca>
2024-09-09 10:16:58 -0400

    Centered the div when the boxes are exactly 16 . Added hover effect to change to blue when its hovered over. Working on next feature.

--- javascript.js
+++ javascript.js
@@ -2,11 +2,13 @@ const container = document.querySelector("#container");
 
 for (let i = 1; i <= 16; i++) {
     const div = document.createElement("div");
-    div.style.backgroundColor = "red";
+    div.style.backgroundColor = "#bde0fe";
     div.style.height = "150px";
-    div.style.width = "150px"
-    div.style.border = "2px solid black"
+    div.style.width = "180px"
+
     div.style.display = "inline-flex";
+    div.style.display = "inline-flex-end"
+    div.style.flexWrap = "no-wrap";
 
     div.classList = "grid"
     container.appendChild(div);
@@ -16,7 +18,7 @@ for (let i = 1; i <= 16; i++) {
 const buttons = document.querySelectorAll(".grid");
 
 buttons.forEach(button => {
-    button.addEventListener("hover", function () {
+    button.addEventListener("mouseover", function () {
         button.style.backgroundColor = "blue";
     })
 
